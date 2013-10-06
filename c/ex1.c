1696bc6c7ed811e5602b8846f2617c574d519bda
o <o@o.(none)>
2013-10-06 16:34:36 +0300

    ex3

--- c/ex1.c
+++ c/ex1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    int i = puts("Hello World.");
-    return i;
+
+    puts("Hello, World!");
+
+    return 0;
 }
 
---
+++ c/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    printf("can I put two %d vars %d here?\n", age, height);
+
+    return 0;
+}
+
