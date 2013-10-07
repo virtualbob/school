091d18efcf9c3db9871ffbd045707600087ea66a
o <o@o.(none)>
2013-10-07 15:31:03 +0300

    lesson 4

---
+++ c/ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* warining: this program contains errors */
+int main ()
+{
+	int age = 10;
+	int height;
+
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
