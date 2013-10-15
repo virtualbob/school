ca84605600cde4933c180bed5abad0d93b641c50
o <o@o.(none)>
2013-10-15 20:34:13 +0300

    lesson 7

--- c/ex6.c
+++ c/ex6.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     char initial = 'A';
     char first_name[] = "Zed";
     char last_name[] = "Shaw";
+    char empty[] = "";
 
     printf("You are %d miles away.\n", distance);
     printf("You have %f levels of power.\n", power);
@@ -16,6 +17,7 @@ int main(int argc, char *argv[])
     printf("I have a last name %s.\n", last_name);
     printf("My whole name is %s %c. %s.\n",
         first_name, initial, last_name);
+    printf("%s", empty);
 
     return 0;
 }
